5645e82de94521b00f4f8553da8d1d25d31b2bd2
padma chockalingam <jmcpadma@gmail.com>
2018-02-09 17:22:15 +0530

    first working commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,44 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { DashboardMComponent } from './components/dashboard-m/dashboard-m.component';
+import { PrintComponent } from './components/print/print.component';
+import { CancelComponent } from './components/cancel/cancel.component';
+import { EditComponent } from './components/edit/edit.component';
+import { ContactComponent } from './components/contact/contact.component';
+
+const routes: Routes = [
+  { 
+    path: 'dashboard',
+    component: DashboardMComponent
+  },
+  {
+    path: 'print',
+    component: PrintComponent
+  },
+  {
+    path: 'cancel',
+    component: CancelComponent
+  },
+  {
+    path: 'edit',
+    component: EditComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent
+  },
+  {
+    path: '', 
+    redirectTo: 'dashboard', 
+    pathMatch: 'full' 
+  },
+  {
+    path: '**', 
+    redirectTo: 'dashboard' , 
+    pathMatch: 'full' 
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
